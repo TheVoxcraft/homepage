e2ff8b61bdd6b8d0b2aa4f9a3d04af109fe3afcf
Jonas <jonasvox.2014@gmail.com>
2022-03-27 13:34:32 +0200

    Update notify_deploy.php

--- public/notify_deploy.php
+++ public/notify_deploy.php
@@ -1,4 +1,17 @@
 <?php
+
+// turn on error reporting
+ini_set('display_errors', 1);
+ini_set('ignore_repeated_errors', TRUE);
+ini_set('error_log', '/var/www/logs/php.log');
+ini_set('log_errors', TRUE);
+error_reporting(E_ALL);
+
+
+// open log file
+$log = fopen("/var/www/logs/log.txt", "a");
+
+
 // get post data
 $postdata = file_get_contents("php://input");
 $request = json_decode($postdata);
@@ -7,10 +20,17 @@ $request = json_decode($postdata);
 $params = array();
 parse_str($postdata, $params);
 
+fwrite($log, date("Y-m-d H:i:s") . $postdata . "\r\n");
+
+// change directory to /home/pi/homepage/
+chdir('/home/pi/homepage/');
+
 // run bash script and return output
-$output = shell_exec('/usr/bin/bash /home/pi/homepage/deploy.sh');
+$output = exec('cd /home/pi/homepage/ && /usr/bin/bash /home/pi/homepage/deploy.sh');
 
-echo $output;
+fwrite($log, date("Y-m-d H:i:s") . $output . "\r\n");
+fclose($log);
 
+echo "Deployed";
 
 ?>
