2c87fd1ac75d6d4c9346ad68d8374663eaea66c7
Jonas <jonasvox.2014@gmail.com>
2022-03-28 15:12:06 +0200

    test deploy

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,8 @@
 cd /var/www/repos/homepage
+echo 'Start git fetch'
 git fetch --progress --all && git reset --hard origin/main
+echo 'Start npm'
 npm install && npm run build
+echo 'Start rsync'
 rsync -r -v dist/* /var/www/html/
 rsync -r -v public/* /var/www/html/
