f3e0ef91bcb984d9da34b1b1fc69527849b2f866
Jonas <jonasvox.2014@gmail.com>
2022-03-28 14:53:30 +0200

    more verbose rsync

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@ cd /var/www/repos/homepage
 git fetch --all && git reset --hard origin/main &&
 npm install &&
 npm run build &&
-rsync -r dist/* /var/www/html/
-rsync -r public/* /var/www/html/
+rsync -r -v dist/* /var/www/html/
+rsync -r -v public/* /var/www/html/
