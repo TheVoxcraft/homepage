03928cc6615e9927529198074f24960a1b9555f4
Jonas <jonasvox.2014@gmail.com>
2022-03-28 15:08:00 +0200

    huh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,5 @@
 cd /var/www/repos/homepage
-git fetch --progress --all && git reset --hard origin/main &&
-npm install &&
-npm run build &&
+git fetch --progress --all && git reset --hard origin/main
+npm install && npm run build
 rsync -r -v dist/* /var/www/html/
 rsync -r -v public/* /var/www/html/
