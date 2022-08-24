a58a9967a96e3ce5893cd76981f669463da852ca
Jonas <jonasvox.2014@gmail.com>
2022-08-24 20:34:35 +0200

    added tracker

--- src/components/HomepageContent.vue
+++ src/components/HomepageContent.vue
@@ -8,6 +8,7 @@ import Link from './Link.vue'
             <p class="welcome-text">Hey</p>
             <Link class="link" href="https://github.com/TheVoxcraft/" text="Github" />
             <Link class="link" href="https://www.linkedin.com/in/jonas-silva-b1a628ba/" text="LinkedIn" />
+            <Link class="link" href="http://tracker.jonaslsa.com/" text="Tracker" />
            <p>hey@jonaslsa.com</p>
         </div>
     </div>
